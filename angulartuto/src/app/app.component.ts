5efd0e36c2727be3a3d42592724030dab5b924a3
christykazi <christyagbai4178@gmail.com>
2024-01-28 23:59:29 +0100

    ui update

--- angulartuto/src/app/app.component.ts
+++ angulartuto/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 styleUrls: ['./app.component.css'],
   standalone: true  // Make sure this line is present
 })
+
 export class AppComponent {
   title = 'angulartuto';
 }
